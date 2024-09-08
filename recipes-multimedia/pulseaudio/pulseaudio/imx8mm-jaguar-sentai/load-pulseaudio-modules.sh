0160d6b7416047a861cbede79647546a1ea2c14d
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2024-09-08 19:07:30 +0100

    Ensure DBUS_SESSION_ADDRESS is setup for pulseaudio
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- recipes-multimedia/pulseaudio/pulseaudio/imx8mm-jaguar-sentai/load-pulseaudio-modules.sh
+++ recipes-multimedia/pulseaudio/pulseaudio/imx8mm-jaguar-sentai/load-pulseaudio-modules.sh
@@ -1,1 +1,13 @@
+#!/bin/bash -e
+user=$(whoami)
+
+fl=$(find /proc -maxdepth 2 -user "$user" -name environ -print -quit)
+for i in {1..5}
+do
+  fl=$(find /proc -maxdepth 2 -user "$user" -name environ -newer "$fl" -print -quit)
+done
+
+export DBUS_SESSION_BUS_ADDRESS
+DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS "$fl" | cut -d= -f2-)
+
 pactl load-module module-native-protocol-unix socket=/tmp/pulseaudio.socket auth-anonymous=true
