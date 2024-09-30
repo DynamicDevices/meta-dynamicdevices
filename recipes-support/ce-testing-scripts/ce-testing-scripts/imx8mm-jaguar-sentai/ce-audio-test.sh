82f5066313b01a146b30726644bbbd17047e3cda
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2024-09-30 12:22:37 +0100

    I don't think audio mixer always works so repeat it
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- recipes-support/ce-testing-scripts/ce-testing-scripts/imx8mm-jaguar-sentai/ce-audio-test.sh
+++ recipes-support/ce-testing-scripts/ce-testing-scripts/imx8mm-jaguar-sentai/ce-audio-test.sh
@@ -2,10 +2,9 @@
 
 echo Running CE audio testing...
 
-amixer -D pulse sset Master 30%
-
 while [ TRUE ]
 do
+  amixer -D pulse sset Master 30%
   paplay /usr/share/ce-testing/PinkPanther60.wav
 done
 
