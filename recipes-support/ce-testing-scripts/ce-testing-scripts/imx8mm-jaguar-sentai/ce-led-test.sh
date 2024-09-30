11a7b0a0f79a89300ea1d069dd30df6a27b689fc
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2024-09-30 13:35:01 +0100

    Fix LED control
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- recipes-support/ce-testing-scripts/ce-testing-scripts/imx8mm-jaguar-sentai/ce-led-test.sh
+++ recipes-support/ce-testing-scripts/ce-testing-scripts/imx8mm-jaguar-sentai/ce-led-test.sh
@@ -2,7 +2,14 @@
 
 echo Running CE LED testing...
 
-path="/sys/class/leds/led${x}"
+x=0
+while [ $x -le 5 ]
+do
+  path="/sys/class/leds/led${x}"
 
-echo "127" > $path/brightness
-echo "0 255 0" > $path/multi_intensity
+  echo "127" > $path/brightness
+
+  echo " 255 0" > $path/multi_intensity
+
+  x=$(( $x + 1 ))
+done
