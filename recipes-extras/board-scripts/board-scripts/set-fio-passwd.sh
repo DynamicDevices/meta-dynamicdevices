8fa35bfa259e21bd868f67d80011c726d1b03989
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2024-11-08 14:47:37 +0000

    Strip SOC UID leading zeroes and convert to lowercase
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- recipes-extras/board-scripts/board-scripts/set-fio-passwd.sh
+++ recipes-extras/board-scripts/board-scripts/set-fio-passwd.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-SERIAL_NUMBER=`cat /sys/devices/soc0/serial_number`
-echo -e -n "fio\n${SERIAL_NUMBER}\n${SERIAL_NUMBER}\n" | passwd fio
+SERIAL_SOURCE=`cat /sys/devices/soc0/serial_number`
+SERIAL=$(sed -e 's/^0*//' ${SERIAL_SOURCE} | tr '[:upper:]' '[:lower:]' | tr -d '\0')
+echo -e -n "fio\n${SERIAL}\n${SERIAL}\n" | passwd fio
