19cfbb908551ae030fd485f96935353acfaf13e4
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2025-03-05 23:52:41 +0000

    Add note on opteedev key fix
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- kas-build-base.sh
+++ kas-build-base.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# TODO: Look at this to fix missing key issue
+#
+#conf/machine/include/lmp-factory-custom.inc:OPTEE_TA_SIGN_KEY = "${TOPDIR}/conf/factory-keys/opteedev.key"
+#lmp-tools/scripts/rotate_ci_keys.sh:openssl genpkey -algorithm RSA -out factory-keys/opteedev.key \
+#lmp-tools/scripts/rotate_ci_keys.sh:openssl req -batch -new -x509 -key factory-keys/opteedev.key -out factory-keys/opteedev.crt
+
 if [ ! -d ~/yocto ]
 then
   mkdir -p -m 777 ~/yocto
