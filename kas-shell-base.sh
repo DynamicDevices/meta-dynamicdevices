57277ab049b90c8ea76346e0e4a3b46343078c0b
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2025-03-07 20:54:30 +0000

    Script to support shelling to dev env
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

---
+++ kas-shell-base.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# TODO: Look at this to fix missing key issue
+#
+#conf/machine/include/lmp-factory-custom.inc:OPTEE_TA_SIGN_KEY = "${TOPDIR}/conf/factory-keys/opteedev.key"
+#lmp-tools/scripts/rotate_ci_keys.sh:openssl genpkey -algorithm RSA -out factory-keys/opteedev.key \
+#lmp-tools/scripts/rotate_ci_keys.sh:openssl req -batch -new -x509 -key factory-keys/opteedev.key -out factory-keys/opteedev.crt
+
+if [ ! -d ~/yocto ]
+then
+  mkdir -p -m 777 ~/yocto
+  mkdir -p -m 777 ~/yocto/downloads
+  mkdir -p -m 777 ~/yocto/persistent
+  mkdir -p -m 777 ~/yocto/sstate
+fi
+kas-container --runtime-args "-v ${HOME}/yocto:/var/cache" shell kas/lmp-dynamicdevices-base.yml
