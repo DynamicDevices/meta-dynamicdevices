95548d5d9228ffd4978ca172cb344a0471af3d08
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2026-09-07 14:50:44 +0100

    test(waydroid): add acceleration evidence checks

--- recipes-support/waydroid/waydroid.bb
+++ recipes-support/waydroid/waydroid.bb
@@ -36,6 +36,7 @@ SRC_URI:append:imx8mm-jaguar-screen = " \
     file://waydroid-zram.service \
     file://waydroid-container-2gb.conf \
     file://waydroid-memory-headroom \
+    file://waydroid-acceleration-check \
 "
 S = "${WORKDIR}/git"
 
@@ -133,6 +134,7 @@ do_install:append:imx8mm-jaguar-screen() {
     install -m 755 ${WORKDIR}/waydroid-net.sh ${D}/usr/lib/waydroid/data/scripts/waydroid-net.sh
     install -Dm0755 ${WORKDIR}/waydroid-zram ${D}${libexecdir}/waydroid-zram
     install -Dm0755 ${WORKDIR}/waydroid-memory-headroom ${D}${libexecdir}/waydroid-memory-headroom
+    install -Dm0755 ${WORKDIR}/waydroid-acceleration-check ${D}${libexecdir}/waydroid-acceleration-check
     install -Dm0644 ${WORKDIR}/waydroid-zram.service \
         ${D}${systemd_system_unitdir}/waydroid-zram.service
     install -d ${D}${systemd_system_unitdir}/waydroid-container.service.d
--- scripts/validation/waydroid-gpu-provision.sh
+++ scripts/validation/waydroid-gpu-provision.sh
@@ -47,4 +47,32 @@ if grep -Fq 'ro.hardware.vulkan' "${config}"; then
     exit 1
 fi
 
+printf '%s\n' \
+    "lxc.mount.entry = ${test_root}/dev/dri/renderD128 dev/dri/renderD128 none bind,create=file 0 0" \
+    'lxc.mount.entry = /dev/dma_heap/system dev/dma_heap/system none bind,create=file 0 0' \
+    'lxc.mount.entry = /dev/dma_heap/linux,cma dev/dma_heap/linux,cma none bind,create=file 0 0' \
+    'lxc.mount.entry = /dev/video2 dev/video2 none bind,create=file 0 0' \
+    > "${test_root}/config_nodes"
+
+printf '%s\n' \
+    '#!/bin/sh' \
+    'case "$*" in' \
+    '  status) echo "Container: RUNNING" ;;' \
+    '  "shell getprop ro.hardware.egl") echo mesa ;;' \
+    '  "shell getprop ro.hardware.gralloc") echo minigbm_gbm_mesa ;;' \
+    '  "shell getprop ro.hardware.vulkan") : ;;' \
+    '  "shell pm list features") echo feature:android.hardware.opengles.aep ;;' \
+    '  "shell dumpsys SurfaceFlinger") echo "GLES: Mesa etnaviv GC7000Lite" ;;' \
+    '  "shell dumpsys media.codec") echo c2.v4l2.avc.decoder ;;' \
+    '  *) exit 1 ;;' \
+    'esac' > "${test_root}/waydroid"
+chmod 0755 "${test_root}/waydroid"
+
+WAYDROID_CONFIG=${config} \
+WAYDROID_LXC_NODES=${test_root}/config_nodes \
+WAYDROID_SYS_DRM_DIR=${test_root}/sys/class/drm \
+WAYDROID_BIN=${test_root}/waydroid \
+WAYDROID_ALLOW_FAKE_DEVICES=1 \
+    sh "${repo_root}/recipes-support/waydroid/waydroid/waydroid-acceleration-check"
+
 echo 'Waydroid Etnaviv GPU and V4L2 provisioning: passed'
